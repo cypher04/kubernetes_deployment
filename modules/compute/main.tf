418a4722016e51e97d8aeccf9b3545480181e415
Seyi Abiodun <cypher04>
2026-03-16 12:32:17 +0000

    RBAC(kubernetes to ACR): Granted pull and push permission to kubernetes

--- modules/compute/main.tf
+++ modules/compute/main.tf
@@ -19,3 +19,15 @@ resource "azurerm_container_registry" "kubernetes" {
 }
 
 
+resource "azurerm_role_assignment" "acr_pull" {
+  scope                = azurerm_container_registry.kubernetes.id
+  role_definition_name = "AcrPull"
+  principal_id         = azurerm_kubernetes_cluster.kubernetes.identity[0].principal_id
+  
+}
+
+resource "azurerm_role_assignment" "acr_push" {
+  scope                = azurerm_container_registry.kubernetes.id
+  role_definition_name = "AcrPush"
+  principal_id         = azurerm_kubernetes_cluster.kubernetes.identity[0].principal_id
+}
