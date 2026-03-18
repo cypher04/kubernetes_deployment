d78a6134eb9ac1eee75961a0287307125a065b0d
Seyi Abiodun <cypher04>
2026-03-18 15:32:55 +0000

    feature(kube_service/deployment): Added kubernetes deployment and service block to the cluster.

--- modules/aks/outputs.tf
+++ modules/aks/outputs.tf
@@ -1,7 +1,7 @@
-output "client_certificate" {
-    value = azurerm_kubernetes_cluster.kubernetes.kube_config.0.client_certificate
-    sensitive = true
-}
+# output "client_certificate" {
+#     value = azurerm_kubernetes_cluster.kubernetes.kube_config.0.client_certificate
+#     sensitive = true
+# }
 
 output "kube_config" {
     value = azurerm_kubernetes_cluster.kubernetes.kube_config_raw
@@ -10,4 +10,32 @@ output "kube_config" {
 
 output "aks_id" {
     value = azurerm_kubernetes_cluster.kubernetes.id
+}
+
+output "kube_principal_id" {
+    value = azurerm_kubernetes_cluster.kubernetes.identity[0].principal_id
+}
+
+output "aks_cluster_name" {
+    value = azurerm_kubernetes_cluster.kubernetes.name
+}
+
+output "host" {
+    value     = azurerm_kubernetes_cluster.kubernetes.kube_config.0.host
+    sensitive = true
+}
+
+output "client_certificate" {
+    value     = azurerm_kubernetes_cluster.kubernetes.kube_config.0.client_certificate
+    sensitive = true
+}
+
+output "client_key" {
+    value     = azurerm_kubernetes_cluster.kubernetes.kube_config.0.client_key
+    sensitive = true
+}
+
+output "cluster_ca_certificate" {
+    value     = azurerm_kubernetes_cluster.kubernetes.kube_config.0.cluster_ca_certificate
+    sensitive = true
 }
